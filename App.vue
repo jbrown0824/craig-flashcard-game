fe3125583e9c3397b8d9c86cea774917e4bdf5d5
Craig Brown 18 <CraigsNewMacBook@Craigs-MBP.lan1>
2019-11-28 17:21:25 -0800

    Flashcard Game

---
+++ App.vue
@@ -0,0 +1,59 @@
+<template>
+<div>
+    <form v-show="showForm" action @submit.prevent="showForm = false">
+    <textarea v-model="text"></textarea>
+    <button type="submit">Show Flash Card</button>
+    </form>
+    <div v-show="!showForm">
+        <a href @click.prevent="showForm = true">Back</a><br>
+        <a href @click.prevent="toggle = !toggle">Toggle</a><br>
+        {{ content }}
+    </div>
+</template>
+
+<style>
+textarea{ min-width: 400px; min-height: 200px; }
+</style>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      text: '',
+      showForm: true,
+      toggle: false,
+    };
+  },
+
+    computed: {
+        content() {
+            if (this.toggle) {
+                return this.text.split(' ').map(word => this.parseWord(word)).join(' ');
+            }
+
+            return this.text;
+        },
+
+    },
+
+    methods: {
+
+        parseWord(word) {
+            let text = word[0];
+            let leftover = word.toString().substr(1);
+            let regex = /([a-zA-Z])/gi;
+            text = text + leftover.replace(regex, '');
+
+            return text;
+        }
+    }
+});
+</script>
+
+<style lang="scss" scoped>
+.container {
+  color: green;
+}
+</style>
